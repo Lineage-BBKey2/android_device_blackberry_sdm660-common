9ade4b5bc72615676f2b231507f57e4889913959
zapaca <zapaca.git@gmail.com>
2026-06-25 14:19:25 -0400

    Keyboard: Add files for keyboard variant detection
    Change-Id: I2e2576b487d2b4c2958f259bb5a49dd3bff84114

--- common.mk
+++ common.mk
@@ -347,6 +347,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
+    init.keypad.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sensors.sh \
